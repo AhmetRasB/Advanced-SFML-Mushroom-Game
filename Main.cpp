fa189d2fa1241d63319d06391e16ceb06952eddd
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2023-12-31 19:46:47 +0300

    .

---
+++ Main.cpp
@@ -0,0 +1,11 @@
+#include "Game.h"
+
+void main(int argc, void** argv[]){
+	// Program entry point.
+	Game game;
+	while(!game.GetWindow()->IsDone()){
+		game.Update();
+		game.Render();
+		game.RestartClock();
+	}
+}
